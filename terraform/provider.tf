c255111be8c9cc2b185272f4eddd83bd074d528a
Felipe Costa <f1.costa2022@gmail.com>
2025-12-28 14:41:00 -0300

    fix: ajusta provider Kubernetes para usar exec auth
    - Usa exec auth do AWS CLI em vez de data sources
    - Evita dependência circular
    - Provider será configurado dinamicamente pelo módulo EKS

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -16,22 +16,23 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Data source para obter informações do cluster EKS
-data "aws_eks_cluster" "this" {
-  name = "eks-devops"
-  depends_on = [module.eks]
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = "eks-devops"
-  depends_on = [module.eks]
-}
-
-# Provider Kubernetes configurado dinamicamente após o cluster ser criado
+# Provider Kubernetes - será configurado dinamicamente pelo módulo EKS
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      module.eks.cluster_name,
+      "--region",
+      "us-east-1"
+    ]
+  }
 }
 
 
