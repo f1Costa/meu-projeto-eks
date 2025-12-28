e91fd80a67e88d0ceb159efdc78e24ddd350430c
Felipe Costa <f1.costa2022@gmail.com>
2025-12-28 14:40:46 -0300

    fix: adiciona provider Kubernetes para gerenciar aws-auth ConfigMap
    - Adiciona provider Kubernetes ao Terraform
    - Configura provider para usar cluster EKS via data sources
    - Resolve erro: connection refused ao validar ConfigMap aws-auth

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,12 +4,34 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"   # 👈 FIXANDO EM 5.x
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23"
+    }
   }
   required_version = ">= 1.6.0"
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+# Data source para obter informações do cluster EKS
+data "aws_eks_cluster" "this" {
+  name = "eks-devops"
+  depends_on = [module.eks]
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = "eks-devops"
+  depends_on = [module.eks]
+}
+
+# Provider Kubernetes configurado dinamicamente após o cluster ser criado
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
 }
 
 
